ef7aebef9470e1836e637468f7f4eb054f3f7b95
Gandein <mazza.mariano@hotmail.com>
2016-07-01 00:41:32 -0300

    ahora tambien con edit!

--- CouchInn/src/AppBundle/Controller/PublicacionController.php
+++ CouchInn/src/AppBundle/Controller/PublicacionController.php
@@ -208,7 +208,13 @@ class PublicacionController extends Controller
             ->getRepository('AppBundle:Publicacion')
             ->find($id);
         $deleteForm = $this->createDeleteForm($publicacion);
-         $editForm = $this->createForm('AppBundle\Form\PublicacionType', $publicacion);
+        if ($this->getUser()->esPremium()) {
+            $editForm = $this->createForm('AppBundle\Form\PublicacionPremiumType', $publicacion);
+        }
+        else
+        {
+            $editForm = $this->createForm('AppBundle\Form\PublicacionType', $publicacion);
+        }
         $editForm->add('reservado', ChoiceType::class, [
             'choices' => [
                 'Reservado' => true,
@@ -229,10 +235,29 @@ class PublicacionController extends Controller
             $foto->move($dir, $name);
             $publicacion->setPath($name);
 
+            if ($this->getUser()->esPremium()) {
+                $foto2 = $editForm['foto2']->getData();
+                $extension2 = $foto2->guessExtension();
+                if (!$extension2) {
+                    $extension2 = 'bin';
+                }
+                $name2 = md5(uniqid()).'.'.$extension2;
+                $foto2->move($dir, $name2);
+                $publicacion->setPath2($name2);
+
+                $foto3 = $editForm['foto3']->getData();
+                $extension3 = $foto3->guessExtension();
+                if (!$extension3) {
+                    $extension3 = 'bin';
+                }
+                $name3 = md5(uniqid()).'.'.$extension3;
+                $foto3->move($dir, $name3);
+                $publicacion->setPath3($name3);
+            }
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($publicacion);
             $em->flush();
-
             return $this->redirectToRoute('_mostrarPublicacion', array('id' => $publicacion->getId()));
         }
 
